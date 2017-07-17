9be8d3aa8d5b870ceecccb8c6ae15269907928a7
ronalddijks <ronalddijks@hoppinger.com>
2017-07-17 15:01:23 +0200

    Added entrypoint for react.

--- Client/custom_views.tsx
+++ Client/custom_views.tsx
@@ -4,3 +4,7 @@ import * as Immutable from "immutable"
 import * as List from './containers/list'
 import * as Models from './generated_models'
 import * as Api from './generated_api'
+
+export let HomePage = function(slug: string) : JSX.Element {
+    return (<div></div>)
+}
