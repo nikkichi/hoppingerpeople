6f826e1b7e3822f7bb2ef6e41f17317ecf4dd6bb
Sarahbel94 <0881357@hr.nl>
2017-07-20 09:59:37 +0200

    Verwisselen van pages (still in progress)

--- Client/custom_views.tsx
+++ Client/custom_views.tsx
@@ -4,7 +4,8 @@ import * as Immutable from "immutable"
 import * as List from './containers/list'
 import * as Models from './generated_models'
 import * as Api from './generated_api'
+import * as Ooievaarspasinfo from './OoievaarsPasInfo'
 
 export let HomePage = function(slug: string) : JSX.Element {
-    return (<div></div>)
+    return (<div><Ooievaarspasinfo.PageManagerComponent/></div>)
 }
